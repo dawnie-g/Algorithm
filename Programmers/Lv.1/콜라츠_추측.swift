fb1020d4bb42d614b6673e4bf1922d11d45c8f57
Dahlia <down022@gmail.com>
2023-04-08 15:49:42 +0900

    solved

---
+++ Programmers/Lv.1/콜라츠_추측.swift
@@ -0,0 +1,18 @@
+func solution(_ num:Int) -> Int {
+    var toOne = num
+    var count = 0
+
+    while toOne != 1 {
+        if count < 500 {
+            count += 1
+        }else {
+            return -1
+        }
+        if toOne % 2 == 0 {
+            toOne /= 2
+        }else {
+             toOne = toOne * 3 + 1      
+        }
+    }
+    return count
+}
