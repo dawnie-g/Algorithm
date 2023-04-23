2563d3b03ec4f0deff0f2bf3b3c066ce80111fa1
Dahlia <down022@gmail.com>
2023-04-23 21:43:22 +0900

    solved

---
+++ Programmers/Lv.1/삼총사.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ number:[Int]) -> Int {
+    var zeroSumThree = 0
+    
+    for i in 0..<number.count {
+        for j in (i+1)..<number.count {
+            for k in (j+1)..<number.count {
+                if [number[i], number[j], number[k]].reduce(0, +) == 0 {
+                    zeroSumThree += 1
+                }
+            }
+        }
+    }
+
+    return zeroSumThree
+}
