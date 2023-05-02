ed167c7938ab27642ebbbdf121e9cfaf4886b49f
Dahlia <down022@gmail.com>
2023-05-02 11:24:29 +0900

    solved

---
+++ Programmers/Lv.2/점프와_순간_이동.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ n:Int) -> Int 
+{
+    var rest = n
+    var ans = 1
+
+    while rest != 1 {
+        if rest%2 == 1 { ans += 1 }
+        rest /= 2
+    }
+
+    return ans
+}
