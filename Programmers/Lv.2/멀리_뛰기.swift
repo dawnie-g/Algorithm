34a395e7f64ae5aa8a7167465fdd061e429d14be
Dahlia <down022@gmail.com>
2023-05-03 13:42:28 +0900

    solved: Fibonacci

---
+++ Programmers/Lv.2/멀리_뛰기.swift
@@ -0,0 +1,13 @@
+func solution(_ n:Int) -> Int {
+    var prev = 1
+    var ans = 0
+    var next = prev + ans
+
+    for _ in 1...n {
+        ans = next
+        next = (ans + prev) % 1234567
+        prev = ans
+    }
+
+    return ans
+}
