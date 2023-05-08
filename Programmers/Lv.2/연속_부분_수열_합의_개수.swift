a23c1ca2f58e7e2cfdae096daf65365a16473d7c
Dahlia <down022@gmail.com>
2023-05-08 23:58:09 +0900

    solved

---
+++ Programmers/Lv.2/연속_부분_수열_합의_개수.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ elements:[Int]) -> Int {
+    let base = elements + elements
+    let count = elements.count
+    var array = elements
+    var ans = array
+    
+    for i in 1..<count {
+        array = zip(array.suffix(count), base[i..<i+count]).map{$0.0 + $0.1}
+    }
+    
+    return Set(ans).count
+}
