838affe9cce91fe0b80740833b991544ce06a119
Dahlia <down022@gmail.com>
2023-05-15 20:24:12 +0900

    sorted Dictionary

--- Programmers/Lv.2/튜플.swift
+++ Programmers/Lv.2/튜플.swift
@@ -1,7 +1,5 @@
 import Foundation
 
 func solution(_ s:String) -> [Int] {
-    let nums = Dictionary (grouping: s.components(separatedBy: "},").flatMap{$0.filter{$0 != "{" && $0 != "}"}.components(separatedBy: ",")}) {$0}
-
-    return nums.sorted{$0.1.count > $1.1.count}.map{Int($0.0)!}
+    Dictionary (grouping: s.components(separatedBy: "},").flatMap{$0.filter{$0 != "{" && $0 != "}"}.components(separatedBy: ",")}) {$0}.sorted{$0.1.count > $1.1.count}.map{Int($0.0)!}
 }
