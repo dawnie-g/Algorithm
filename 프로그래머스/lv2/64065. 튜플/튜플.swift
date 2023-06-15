0786b0f50e3cc597ec771e6bd210e31cf22b3c8c
KwakDADA <down022@gmail.com>
2023-06-16 00:41:46 +0900

    [level 2] Title: 튜플, Time: 0.22 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ s:String) -> [Int] {
+    var components = [Int: Int]()
+    s.split { !$0.isNumber }.map { Int(String($0))! }.forEach {
+        components[$0, default: 0] += 1
+    }
+
+    return components.sorted { $0.value > $1.value }.map { $0.key }
+}
