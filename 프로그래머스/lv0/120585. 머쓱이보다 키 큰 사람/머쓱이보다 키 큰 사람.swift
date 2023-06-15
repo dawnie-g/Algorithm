8a070671bcf1372fc865b3d883c835101b908219
KwakDADA <down022@gmail.com>
2023-06-15 23:16:11 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ array:[Int], _ height:Int) -> Int {
+    var count: Int = 0
+    for i in array where height < i{
+        count += 1
+    }
+    
+    return count
+}
