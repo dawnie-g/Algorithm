5f1f93cf95d83b6ef8db6115c85def306b37ecf1
Dahlia <down022@gmail.com>
2023-06-15 17:03:51 +0900

    break outerLoop

--- BAEKJOON/2309_일곱_난쟁이.playground/Contents.swift
+++ BAEKJOON/2309_일곱_난쟁이.playground/Contents.swift
@@ -1,21 +1,14 @@
-var height = [Int]()
-
-for _ in 1...9 {
-    height.append(Int(readLine()!)!)
-}
-
-var gameover = false
+var height = (0..<9).map{_ in Int(readLine()!)!}
 var liar: [Int] = [0, 0]
-for i in 0...8 {
+
+outerLoop: for i in 0...8 {
     liar[0] = height[i]
     for j in i+1...8 {
         liar[1] = height[j]
         
         if height.filter{!liar.contains($0)}.reduce(0, +) == 100 {
             print(height.filter{!liar.contains($0)}.sorted().map{String($0)}.joined(separator: "\n"))
-            gameover = true
-            break
+            break outerLoop
         }
     }
-    if gameover { break }
 }
