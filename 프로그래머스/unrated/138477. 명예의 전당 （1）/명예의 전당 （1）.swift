524a199a6ca26eaab1434fca71b0e8042b65c1ea
KwakDADA <down022@gmail.com>
2023-06-16 00:41:19 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ k:Int, _ score:[Int]) -> [Int] {
+    var pawn: [Int] = []
+    var result: [Int] = []
+    
+    for s in score {
+        pawn.append(s)
+        pawn.sort(by: >)
+        if pawn.count > k {
+            pawn.removeLast()
+        }
+        result.append(pawn.last!)
+    }
+    
+    return result
+}
