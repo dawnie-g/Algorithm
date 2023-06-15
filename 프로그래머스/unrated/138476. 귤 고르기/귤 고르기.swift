97dc85d7400357e8301859133b6e730722a799c9
KwakDADA <down022@gmail.com>
2023-06-16 00:38:22 +0900

    [unrated] Title: 귤 고르기, Time: 354.85 ms, Memory: 24.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.swift
@@ -0,0 +1,6 @@
+func solution(_ k: Int, _ tangerine: [Int]) -> Int {
+    Dictionary(grouping: tangerine) { $0 }.values
+        .sorted { $0.count > $1.count }
+        .reduce((0, 0)) { acc, array in acc.1 >= k ? acc : (acc.0 + 1, acc.1 + array.count) }
+        .0
+}
