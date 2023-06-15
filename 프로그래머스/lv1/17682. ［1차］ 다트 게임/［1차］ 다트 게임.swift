a76caaadf47bed69122cca1b71453f22471b399e
KwakDADA <down022@gmail.com>
2023-06-16 00:48:38 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.08 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+func solution(_ dartResult:String) -> Int {
+    var score = [Int]()
+    var endIndex = 0
+
+    for c in dartResult {
+        if c.isNumber {
+            if score.isEmpty && c == "0" {
+                score.append(0)
+            } else if c == "0" && score[endIndex] == 1 {
+                score[endIndex] = 10
+            } else {
+                score.append(Int(String(c))!)
+            }
+            endIndex = score.endIndex-1
+        } else {
+            switch c {
+            case "S": 
+                continue
+            case "D": 
+                score[endIndex] *= score[endIndex]
+            case "T": 
+                score[endIndex] *= (score[endIndex] * score[endIndex])
+            case "*":
+                score[endIndex] *= 2
+                if endIndex > 0 {
+                    score[endIndex-1] *= 2
+                }
+            case "#": 
+                score[endIndex] *= -1
+            default : 
+                continue
+            }
+        }
+    }
+    return score.reduce(0, +)
+}
