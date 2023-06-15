1a2ae0b1a94e09993705c07d02921a097da8343f
KwakDADA <down022@gmail.com>
2023-06-15 22:54:39 +0900

    [Bronze II] Title: 숫자, Time: 32 ms, Memory: 73024 KB -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.swift
@@ -0,0 +1,5 @@
+let n = readLine()!.split(separator: " ").map{Int($0)!}.sorted()
+print(n[0] == n[1] ? 0 : n[1]-n[0]-1)
+if n[1]-n[0]-1 > 0 {
+    print((1...n[1]-n[0]-1).map{String(n[0] + $0)}.joined(separator: " "))
+}
