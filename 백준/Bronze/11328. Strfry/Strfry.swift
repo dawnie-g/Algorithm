a06ffb96c9d926b57b30e074b309645ff724857c
KwakDADA <down022@gmail.com>
2023-06-19 14:15:05 +0900

    [Bronze II] Title: Strfry, Time: 84 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.swift
+++ 백준/Bronze/11328. Strfry/Strfry.swift
@@ -1,5 +1,12 @@
 let n = Int(readLine()!)!
+
 for _ in 1...n {
-    let line = readLine()!.split(separator: " ").map{$0.sorted()}
-    print(line[0] == line[1] ? "Possible" : "Impossible")
+    let line = readLine()!.split(separator: " ").map{String($0)}
+    var cnt1 = [Int](repeating: 0, count: 200)
+    var cnt2 = [Int](repeating: 0, count: 200)
+    
+    line[0].forEach { cnt1[Int($0.asciiValue!)] += 1 }
+    line[1].forEach { cnt2[Int($0.asciiValue!)] += 1 }
+    
+    print(cnt1 == cnt2 ? "Possible" : "Impossible")
 }
