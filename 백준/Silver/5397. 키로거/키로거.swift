1ff732f8fe65bb47448b4a893f49d489aad8e5bd
KwakDADA <down022@gmail.com>
2023-06-20 21:18:44 +0900

    [Silver II] Title: 키로거, Time: 596 ms, Memory: 109872 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.swift
+++ 백준/Silver/5397. 키로거/키로거.swift
@@ -1,3 +1,4 @@
+
 let n = Int(readLine()!)!
 
 for _ in 0..<n {
@@ -6,21 +7,18 @@ for _ in 0..<n {
     var right = [Character]()
     
     for input in line {
-        if input.isLetter || input.isNumber {
+        switch input {
+        case "-":
+            guard !left.isEmpty else { continue }
+            left.removeLast()
+        case "<":
+            guard !left.isEmpty else { continue }
+            right.append(left.removeLast())
+        case ">":
+            guard !right.isEmpty else { continue }
+            left.append(right.removeLast())
+        default: 
             left.append(input)
-        } else {
-            switch input {
-            case "-":
-                guard !left.isEmpty else { continue }
-                left.removeLast()
-            case "<":
-                guard !left.isEmpty else { continue }
-                right.append(left.removeLast())
-            case ">":
-                guard !right.isEmpty else { continue }
-                left.append(right.removeLast())
-            default: break
-            }
         }
     }
     print(String(left+right.reversed()))
