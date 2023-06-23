e19c1490ec74ffc74be56feaa70b7b84673102b7
KwakDADA <down022@gmail.com>
2023-06-23 12:16:39 +0900

    [Silver III] Title: 회전하는 큐, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.swift
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.swift
@@ -1,28 +1,18 @@
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let n = input[0]
 let target = readLine()!.split(separator: " ").map{Int(String($0))!}
-
 var queue = Array(1...n)
 var pointer = 0
-var count = 0
+var size = n
 var ans = 0
 
-while count < input[1] {
-    if queue[pointer] == target[count] {
-        queue.remove(at: pointer)
-        count += 1
-        pointer = pointer == queue.count ? 0 : pointer
-    } else {
-        let idx = queue.firstIndex(of: target[count])!
-        let right = idx > pointer ? idx - pointer : queue.count + idx - pointer
-        let left = queue.count - right
-        if right <= left { // target is closer to right
-            pointer = (pointer + 1) % queue.count
-            ans += 1
-        } else { // target is closer to left
-            pointer = pointer == 0 ? queue.count - 1 : pointer - 1
-            ans += 1
-        }
-    }
+for key in target {
+    let keyIdx = queue.firstIndex(of: key)!
+    var d = abs(pointer - keyIdx)
+    d = min(d, size-d)
+    pointer = keyIdx
+    queue.remove(at: pointer)
+    ans += d
+    size -= 1
 }
 print(ans)
