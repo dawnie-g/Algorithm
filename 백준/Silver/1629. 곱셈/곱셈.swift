27cde2b38dc3ab9491c142486851dd0cf3fdbcbe
KwakDADA <down022@gmail.com>
2023-07-02 15:50:00 +0900

    [Silver I] Title: 곱셈, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.swift
+++ 백준/Silver/1629. 곱셈/곱셈.swift
@@ -1,12 +1,12 @@
 let abc = readLine()!.split(separator: " ").map{Int(String($0))!}
 let a = abc[0], b = abc[1], c = abc[2]
 
-func multi(_ a: Int, _ b: Int, _ c: Int) -> Int {
+func multi(_ b: Int) -> Int {
     if b == 1 { return a % c }
-    var val = multi(a, b/2, c)
+    var val = multi(b/2)
     val = val * val % c
     if b % 2 == 0 { return val }
     return val * a % c
 }
 
-print(multi(a, b, c))
+print(multi(b))
