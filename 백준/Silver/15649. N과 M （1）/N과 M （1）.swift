60722f3dde7323d1393febd0c9bccae2ab26a065
KwakDADA <down022@gmail.com>
2023-07-05 16:26:59 +0900

    [Silver III] Title: N과 M (1), Time: 88 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
@@ -1,20 +1,21 @@
 let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
 let n = nm[0], m = nm[1]
-var arr = [Int](repeating: 0, count: m)
+var output = ""
 var issued = [Bool](repeating: false, count: n+1)
 
 func bt(_ k: Int) {
     if k == m {
-        print(arr.map{String($0)}.joined(separator: " "))
+        print(output.dropLast(1))
         return
     }
     
     for i in 1...n {
         if !issued[i] {
-            arr[k] = i
+            output.append("\(i) ")
             issued[i] = true
             bt(k+1)
             issued[i] = false
+            output.removeLast(2)
         }
     }
 }
