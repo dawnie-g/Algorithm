2f986d551db9951e1cff6d3a2d3a68b80b3a603e
KwakDADA <down022@gmail.com>
2023-07-07 11:18:55 +0900

    [Silver III] Title: N과 M (7), Time: 444 ms, Memory: 132092 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.swift
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.swift
@@ -2,22 +2,17 @@ let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
 let num = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted()
 let n = nm[0], m = nm[1]
 
-var arr = [String]()
 var str = ""
-
-func backTracking(_ k: Int) {
+func backTracking(_ k: Int, _ line: String) {
     if k == m {
-        str += arr.joined(separator: " ")
-        str += "\n"
+        str += line + "\n"
         return
     }
     
     for i in 0..<n {
-        arr.append(String(num[i]))
-        backTracking(k+1)
-        arr.removeLast()
+        backTracking(k + 1, line + "\(num[i]) ")
     }
 }
 
-backTracking(0)
+backTracking(0, "")
 print(str)
