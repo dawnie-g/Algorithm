04c149fa2ddb46b97de03f2c90b93a0c5a673e83
KwakDADA <down022@gmail.com>
2023-07-10 00:07:07 +0900

    [Gold V] Title: 암호 만들기, Time: 24 ms, Memory: 69112 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.swift
@@ -0,0 +1,27 @@
+let lc = readLine()!.split(separator: " ").map{Int(String($0))!}
+let l = lc[0], c = lc[1]
+let letters = readLine()!.split(separator: " ").map{Character(String($0))}.sorted()
+
+var isUsed = [Bool](repeating: false, count: c)
+var aeiou = 0
+let vowel: [Character] = ["a", "e", "i", "o", "u"]
+var ans = ""
+
+func backTracking(_ depth: Int, _ line: String, _ prev: Character) {
+    guard l - aeiou > 1 else { return }
+    if depth == l && aeiou > 0 {
+        ans += line + "\n"
+        return
+    }
+    for i in 0..<c where !isUsed[i]{
+        guard line.isEmpty || letters[i].asciiValue! > prev.asciiValue! else { continue }
+        if vowel.contains(letters[i]) { aeiou += 1 }
+        isUsed[i] = true
+        backTracking(depth+1, line + "\(letters[i])", letters[i])
+        isUsed[i] = false
+        if vowel.contains(letters[i]) { aeiou -= 1 }
+    }
+}
+
+backTracking(0, "", "_")
+print(ans)
