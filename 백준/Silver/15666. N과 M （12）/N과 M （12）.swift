3411aa939cb18ae9a3cb6e7a23b91926731fe616
KwakDADA <down022@gmail.com>
2023-07-09 15:57:40 +0900

    [Silver II] Title: N과 M (12), Time: 8 ms, Memory: 69276 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.swift
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.swift
@@ -2,23 +2,18 @@ let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
 let num = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted()
 let n = nm[0], m = nm[1]
 
-var remain = [Int](repeating: 0, count: 10_001)
-for i in num {
-    remain[i] += 1
-}
 var str = ""
 
-func backTracking(_ k: Int, _ line: [Int]) {
+func backTracking(_ k: Int, _ line: String, _ prev: Int) {
     if k == m {
-        str += line.map{String($0)}.joined(separator: " ")
-        str += "\n"
+        str += line + "\n"
         return
     }
     for i in 0..<n {
         guard i == 0 || (i > 0 && num[i-1] != num[i]) else { continue }
-        guard line.isEmpty || (!line.isEmpty && line.last! <= num[i]) else { continue }
-        backTracking(k+1, line + [num[i]])
+        guard prev <= num[i] else { continue }
+        backTracking(k+1, line + "\(num[i]) ", num[i])
     }
 }
-backTracking(0, [])
+backTracking(0, "", 0)
 print(str)
