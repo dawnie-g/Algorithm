874b254089540ff3a834cb55c917b722d2bad911
KwakDADA <down022@gmail.com>
2023-09-07 09:50:50 +0900

    [Silver IV] Title: 보물, Time: 8 ms, Memory: 69112 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.swift
+++ 백준/Silver/1026. 보물/보물.swift
@@ -1,10 +1,10 @@
-let n = Int(readLine()!)
+let n = Int(readLine()!)!
 var A = readLine()!.split(separator: " ").map{Int($0)!}.sorted()
 var B = readLine()!.split(separator: " ").map{Int($0)!}.sorted(by: >)
 var ans = 0
 
-for (a, b) in zip(A, B) {
-    ans += a * b
+for i in 0..<n {
+    ans += A[i] * B[i]
 }
 
 print(ans)
