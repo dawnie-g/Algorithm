e72d4a3aac189e284de3ab7e03b722e9a85ef674
KwakDADA <down022@gmail.com>
2023-10-07 19:29:02 +0900

    [level 3] Title: 숫자 게임, Time: 442.19 ms, Memory: 24.7 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.swift
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.swift
@@ -3,17 +3,13 @@ import Foundation
 func solution(_ a:[Int], _ b:[Int]) -> Int {
     let a = a.sorted(by: >)
     let b = b.sorted(by: >)
-    var score = 0
-    var ai = 0
-    var bi = 0
+    var i = 0
     
-    while bi < b.count && ai < a.count {
-        if a[ai] < b[bi] {
-            score += 1
-            bi += 1
+    for n in a {
+        if n < b[i] {
+            i += 1
         }
-        ai += 1
     }
     
-    return score
+    return i
 }
