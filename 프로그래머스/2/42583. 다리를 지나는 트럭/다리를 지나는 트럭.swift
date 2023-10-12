1c902e4777b2c8afb29d76a246abddabe548eef6
KwakDADA <down022@gmail.com>
2023-10-12 15:07:05 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.10 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
@@ -2,26 +2,30 @@ import Foundation
 
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
     var waiting = Array(truck_weights.reversed())
-    var onBridge = [Int](repeating: 0, count: bridge_length)
-    var sumOfWeight = 0
+    var onBridge = [Int]()
+    var head = 0
+    var tail = 0
+    
+    var sumOfWeights = 0
     var cnt = 0
-    var time = 0
     
     while cnt < truck_weights.count {
-        if onBridge.first! != 0 {
-            sumOfWeight -= onBridge.first!
-            cnt += 1
+        if !onBridge.isEmpty && tail - head == bridge_length {
+            if onBridge[head] != 0 {
+                cnt += 1
+            }
+            sumOfWeights -= onBridge[head]
+            head += 1
         }
-        onBridge.removeFirst()
-        time += 1
         
-        if !waiting.isEmpty && sumOfWeight + waiting.last! <= weight {
-            sumOfWeight += waiting.last!
+        if tail - head < bridge_length && !waiting.isEmpty && sumOfWeights + waiting.last! <= weight {
+            sumOfWeights += waiting.last!
             onBridge.append(waiting.removeLast())
         } else {
             onBridge.append(0)
         }
+        tail += 1
     }
     
-    return time
+    return tail
 }
