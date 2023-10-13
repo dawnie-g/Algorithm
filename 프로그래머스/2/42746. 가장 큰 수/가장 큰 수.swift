f81116a6c0d13407a5c340d1245c5fb0becfedf8
KwakDADA <down022@gmail.com>
2023-10-13 13:46:21 +0900

    [level 2] Title: 가장 큰 수, Time: 0.14 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> String {
-    guard numbers.filter{$0 != 0}.count > 0 else { return "0" }
-    return numbers.map{String($0)}.sorted {$0 + $1 > $1 + $0}.joined()
+    let sorted = numbers.map{String($0)}.sorted {$0 + $1 > $1 + $0}.joined()
+    return sorted.first! == "0" ? "0" : sorted
 }
