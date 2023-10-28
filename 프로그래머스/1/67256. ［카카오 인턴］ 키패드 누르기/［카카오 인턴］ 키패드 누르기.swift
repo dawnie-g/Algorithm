45a4d10412db4a1c467e41002c827b57a732e377
KwakDADA <down022@gmail.com>
2023-10-28 19:58:51 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.16 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.swift
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.swift
@@ -1,60 +1,77 @@
-import Foundation
+func countKeypad(_ start: Int , _ dest: Int) -> Int {
+    // [ 1, 2,  3]
+    // [ 4, 5,  6]
+    // [ 7, 8,  9]
+    // [-1, 0, -2]
+    let keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [-1, 0, -2]]
+    var startPos = [0, 0]
+    var destPos = [0, 0]
 
-func solution(_ numbers:[Int], _ hand:String) -> String {
-    var handOrder = ""
-    var L: Int?
-    var R: Int?
-    
-    for number in numbers {
-        switch number {
-        case 1, 4, 7:
-            handOrder.append("L")
-            L = number
-        case 3, 6, 9:
-            handOrder.append("R")
-            R = number
-        default:
-            let numberLine = number == 0 ? 3 : (number-1) / 3
-            let lLine = (L == 0 || L == nil) ? 3 : (L!-1) / 3
-            let rLine = (R == 0 || R == nil) ? 3 : (R!-1) / 3
-            
-            let n = number == 0 ? 11 : number
-            var LD: Int {
-                if [2, 5, 8].contains(L) {
-                    return abs(n - L!) / 3
-                } else if L == 0 {
-                    return (11 - n) / 3
-                } else {
-                    return abs(lLine - numberLine) + 1
-                }
+    for i in 0..<4 {
+        for j in 0..<3 {
+            if keypad[i][j] == start {
+                startPos[0] = i
+                startPos[1] = j
             }
-            
-            var RD: Int {
-                if [2, 5, 8].contains(R) {
-                    return abs(n - R!) / 3
-                } else if R == 0 {
-                    return (11 - n) / 3
-                } else {
-                    return abs(rLine - numberLine) + 1
-                }
-            }
-            
-            if LD == RD {
-                if hand == "left" {
-                    L = number
-                } else {
-                    R = number
-                }
-                handOrder.append(hand.first!.uppercased())
-            } else if LD < RD {
-                L = number
-                handOrder.append("L")
-            } else {
-                R = number
-                handOrder.append("R")
+            if keypad[i][j] == dest {
+                destPos[0] = i
+                destPos[1] = j
             }
         }
     }
-    
-    return handOrder
+
+    var count = 0
+    for i in 0...1 {
+        count += startPos[i] > destPos[i] ? startPos[i] - destPos[i] : destPos[i] - startPos[i]
+    }
+
+    return count
+}
+
+func solution(_ numbers:[Int], _ hand:String) -> String {
+    var LH = -1
+    var RH = -2
+    var result = ""
+
+    for n in numbers {
+        switch n {
+        case 1, 4, 7:
+            LH = n
+            result += "L"
+            break
+
+        case 3, 6, 9:
+            RH = n
+            result += "R"
+            break
+
+        default:
+            let countLH = countKeypad(LH, n)
+            let countRH = countKeypad(RH, n)
+
+            // 같은거리면 왼손잡이 오른손잡이로 누름
+            if countLH == countRH {
+                if hand == "left" {
+                    LH = n
+                    result += "L"
+                } else {
+                    RH = n
+                    result += "R"
+                }
+                break
+            }
+
+            // 가까운 손으로 누름
+            if countLH < countRH {
+                LH = n
+                result += "L"
+            } else {
+                RH = n
+                result += "R"
+            }
+            break
+        }
+    }
+
+    return result
 }
