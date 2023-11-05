3c122de8c800e432313cf0bd92a667ac78b18973
KwakDADA <down022@gmail.com>
2023-11-05 15:43:11 +0900

    [Silver V] Title: 나이순 정렬, Time: 268 ms, Memory: 90288 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.swift
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.swift
@@ -1,14 +1,74 @@
 import Foundation
 
-let n = Int(readLine()!)!
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let file = FileIO()
+let n = file.readInt()
 var member = [[String]]()
+var str = ""
 
 for _ in 0..<n {
-    member += [readLine()!.split(separator: " ").map{String($0)}]
+    member.append([file.readString(), file.readString()])
 }
 
 member.sort { Int($0[0])! < Int($1[0])! }
 
 for m in member {
-    print(m[0], m[1])
+    str += "\(m[0]) \(m[1])\n"
 }
+
+print(str)
