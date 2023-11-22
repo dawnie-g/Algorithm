04555b74544e9ae8f9fefa67a05e40defb2af571
KwakDADA <down022@gmail.com>
2023-11-22 15:06:58 +0900

    [level 2] Title: 괄호 변환, Time: 3.44 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+func solution(_ p:String) -> String {
+    var v = p
+    
+    func recursive(v: String) -> String {
+        guard !v.isEmpty else { return "" }
+        var u = ""
+        var v = v
+        var openCnt = 0
+        var closeCnt = 0
+        
+        while openCnt != closeCnt || openCnt == 0 {
+            u.append(v.removeFirst())
+            if u.last! == "(" {
+                openCnt += 1
+            } else {
+                closeCnt += 1
+            }
+        }
+        
+        if u.first! == "(" && u.last! == ")" {
+            return u + recursive(v: v)
+        } else {
+            var str = "("
+            str += recursive(v: v)
+            str += ")"
+            
+            u.removeFirst()
+            u.removeLast()
+            for letter in u {
+                if letter == "(" {
+                    str += ")"
+                } else {
+                    str += "("
+                }
+            }
+            return str
+        } 
+    }
+    return recursive(v: v)
+}
