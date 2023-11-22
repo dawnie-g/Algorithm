80745a0b5bacd08fc9068e31926f9920be1e51eb
KwakDADA <down022@gmail.com>
2023-11-22 13:46:57 +0900

    [level 2] Title: 호텔 대실, Time: 25.21 ms, Memory: 16.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+func solution(_ book_time:[[String]]) -> Int {
+    let book_time = book_time.map{$0.map{Int($0.replacingOccurrences(of: ":", with: ""))!}}.sorted { ($0[0], $0[1]) < ($1[0], $1[1]) }
+    
+    print(book_time)
+    var rooms = [[Int]]()
+    
+    outer: for guest in book_time {
+        var start = guest[0]
+        var end = guest[1]
+        
+        if end % 100 > 50 {
+            end += 50
+        } else {
+            end += 10
+        }
+        
+        for i in 0..<rooms.count {
+            if start >= rooms[i][1] {
+                rooms[i][1] = end
+                continue outer
+            }
+        }
+        
+        rooms.append([start, end])
+    }
+    
+    return rooms.count
+}
