c3dbda0e03725620ebd9ca417d62df6f75310d69
다은 <down022@gmail.com>
2024-06-04 10:37:26 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.swift
@@ -0,0 +1,87 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fIO = FileIO()
+let n = fIO.readInt()
+var arr = [Int](repeating: 0, count: n + 1)
+var dp = arr
+var tail = 1
+
+for _ in 1...n {
+    let val = fIO.readInt()
+    if val >= arr[tail-1] {
+        arr[tail] = val
+        if val > arr[tail-1] {
+            dp[tail] = dp[tail-1] + 1
+            tail += 1
+        } else {
+            arr[tail] = val
+            dp[tail] = dp[tail-1]
+        }
+    } else {
+        for j in 0..<tail where val > arr[j] && val < arr[j+1] {
+            arr[j + 1] = val
+        }
+    }
+}
+
+print(tail-1)
