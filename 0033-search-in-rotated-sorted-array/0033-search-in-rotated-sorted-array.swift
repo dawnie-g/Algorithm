ba1e0be317abb0d664e796f1ac5d260e27e2bb2b
다은 <down022@gmail.com>
2024-06-21 04:11:01 +0900

    Time: 12 ms (9.25%), Space: 16.2 MB (5.16%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.swift
@@ -0,0 +1,26 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        guard nums.contains(target) else { return -1 }
+
+        let sortedNums = nums.enumerated().sorted { $0.1 < $1.1 }
+        var low = 0
+        var high = nums.count - 1
+
+        while low <= high {
+            let midIdx = (low + high) / 2
+            let mid = sortedNums[midIdx]
+
+            if mid.1 == target {
+                return mid.0
+            } else if mid.1 < target {
+                low = midIdx + 1
+                print(mid.0, mid.1, low)
+            } else {
+                high = midIdx - 1
+            }
+            print(low, high)
+        }
+
+        return sortedNums[low].1 == target ? sortedNums[low].0 : -1
+    }
+}
