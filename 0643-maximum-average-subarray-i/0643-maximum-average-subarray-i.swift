279008b29c93a7ace592474dfce7e46d498c423e
다은 <down022@gmail.com>
2024-06-25 21:41:33 +0900

    Time: 452 ms (61.63%), Space: 19.9 MB (57.4%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.swift
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.swift
@@ -1,24 +1,18 @@
 class Solution {
     func findMaxAverage(_ nums: [Int], _ k: Int) -> Double {
-        var head = 0
-        var tail = k
-        var currSum = nums[head..<tail].reduce(0, +)
+        var currSum = nums[0..<k].reduce(0, +)
         var maxSum = currSum
         let n = nums.count
 
-        for _ in k..<n {
-            currSum -= nums[head]
-            currSum += nums[tail]
-            head += 1
-            tail += 1
+        for i in 0..<n-k {
+            currSum -= nums[i]
+            currSum += nums[i + k]
 
             if maxSum < currSum {
                 maxSum = currSum
             }
         }
 
-        let average: Double = Double(maxSum) / Double(k)
-        let formattedAvg = round(average * 100000) / 100000
-        return formattedAvg
+        return Double(maxSum) / Double(k)
     }
 }
