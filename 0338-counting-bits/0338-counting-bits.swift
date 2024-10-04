14de698568beab7ab3fc5948b751cc87267436a1
다은 <down022@gmail.com>
2024-10-04 16:13:35 +0900

    Time: 30 ms (91.2%), Space: 21.7 MB (16.35%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.swift
+++ 0338-counting-bits/0338-counting-bits.swift
@@ -1,9 +1,8 @@
 class Solution {
     func countBits(_ n: Int) -> [Int] {
-        guard n > 0 else { return [0] }
         var dp = [Int](repeating: 0, count: n + 1)
         var offset = 1
-
+        if n == 0 { return [0] }
         for i in 1...n {
             if offset * 2 == i {
                 offset = i
