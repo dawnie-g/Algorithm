7cc2f3ecae6b5a67d959f07d5a01da185a7d6c05
다은 <down022@gmail.com>
2024-10-05 12:09:43 +0900

    Time: 124 ms (84.62%), Space: 17.9 MB (7.69%) - LeetHub

--- 0753-open-the-lock/0753-open-the-lock.swift
+++ 0753-open-the-lock/0753-open-the-lock.swift
@@ -1,50 +1,41 @@
 class Solution {
     func openLock(_ deadends: [String], _ target: String) -> Int {
         let deadends = Set(deadends)
-        guard !deadends.contains("0000") else { return -1 }
-        
+        guard !deadends.contains ("0000") else { return -1 }
         let target = target.map { Int(String($0))! }
         let direction = [
-            [1, 0, 0, 0], [-1, 0, 0, 0], 
-            [0, 1, 0, 0], [0, -1, 0, 0], 
-            [0, 0, 1, 0], [0, 0, -1, 0], 
-            [0, 0, 0, 1], [0, 0, 0, -1]
-        ]
+            [1, 0, 0, 0], [-1, 0, 0, 0], [0, 1, 0, 0], [0, -1, 0, 0], 
+            [0, 0, 1, 0], [0, 0, -1, 0], [0, 0, 0, 1], [0, 0, 0, -1]
+            ]
         var visited = [Bool](repeating: false, count: 10000)
         var step = 0
 
         var queue = [[0, 0, 0, 0]]
+        var head = 0
+        var tail = 1
         
-        while !queue.isEmpty {
-            let currentLevelCount = queue.count
-            
-            for _ in 0..<currentLevelCount {
-                let curr = queue.removeFirst()
-                
+        while head != tail {
+            let size = tail - head
+            for _ in 0..<size {
+                let curr = queue[head]
                 if curr == target { return step }
-                
-                let idx = arrToIdx(curr)
-                visited[idx] = true
+                head += 1
+                visited[arrToIdx(curr)] = true
 
                 for dir in direction {
                     let arr = (0...3).map { (curr[$0] + dir[$0] + 10) % 10 }
-                    let idx = arrToIdx(arr)
-                    
-                    if visited[idx] || deadends.contains(arr.map { String($0) }.joined()) {
-                        continue
-                    }
-                    
-                    visited[idx] = true
+                    if visited[arrToIdx(arr)] || deadends.contains(arr.map { String($0) }.joined()) { continue }
+                    visited[arrToIdx(arr)] = true
                     queue.append(arr)
+                    tail += 1
                 }
             }
             step += 1
         }
-        
         return -1
     }
-    
-    func arrToIdx(_ arr: [Int]) -> Int {
-        return arr[0] * 1000 + arr[1] * 100 + arr[2] * 10 + arr[3]
-    }
+}
+
+func arrToIdx(_ arr: [Int]) -> Int {
+    return arr[0] * 1000 + arr[1] * 100 + arr[2] * 10 + arr[3]
 }
