9c334aa0452fcdbbe91df133c64e45214baae6b5
다은 <down022@gmail.com>
2024-10-05 12:01:03 +0900

    Time: 687 ms (15.38%), Space: 17.4 MB (30.77%) - LeetHub

--- 0753-open-the-lock/0753-open-the-lock.swift
+++ 0753-open-the-lock/0753-open-the-lock.swift
@@ -1,13 +1,12 @@
 class Solution {
     func openLock(_ deadends: [String], _ target: String) -> Int {
-        let deadends = Set(deadends)
         guard !deadends.contains ("0000") else { return -1 }
         let target = target.map { Int(String($0))! }
         let direction = [
             [1, 0, 0, 0], [-1, 0, 0, 0], [0, 1, 0, 0], [0, -1, 0, 0], 
             [0, 0, 1, 0], [0, 0, -1, 0], [0, 0, 0, 1], [0, 0, 0, -1]
             ]
-        var visited = Set<Int>()
+        var visited = [Bool](repeating: false, count: 10000)
         var step = 0
 
         var queue = [[0, 0, 0, 0]]
@@ -19,14 +18,14 @@ class Solution {
                 let curr = queue[head]
                 if curr == target { return step }
                 head += 1
-                visited.insert(arrToIdx(curr))
+                visited[arrToIdx(curr)] = true
 
                 for dir in direction {
                     let arr = (0...3).map { (curr[$0] + dir[$0] + 10) % 10 }
                     let str = arr.map { String($0) }.joined()
 
-                    if !visited.contains(arrToIdx(arr)) && !deadends.contains(str) {
-                        visited.insert(arrToIdx(arr))
+                    if !visited[arrToIdx(arr)] && !deadends.contains(str) {
+                        visited[arrToIdx(arr)] = true
                         queue.append(arr)
                         tail += 1
                     }
