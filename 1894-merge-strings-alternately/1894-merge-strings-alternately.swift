234c892bfb7bc91de39b6267b4eec748d11c0e41
다은 <down022@gmail.com>
2024-10-08 08:27:11 +0900

    Time: 7 ms (42.68%), Space: 16.6 MB (16.38%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.swift
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.swift
@@ -1,22 +1,35 @@
 class Solution {
     func mergeAlternately(_ word1: String, _ word2: String) -> String {
-        let word1 = word1.map { $0 }
-        let word2 = word2.map { $0 }
-        let size1 = word1.count
-        let size2 = word2.count
-        let minSize = min(size1, size2)
+        var arr1: [Character] = []
+        var arr2: [Character] = []
+        var size1 = 0
+        var size2 = 0
+
+        word1.forEach {
+            arr1.append($0)
+            size1 += 1
+        }
+
+        word2.forEach {
+            arr2.append($0)
+            size2 += 1
+        }
+
         var ans = ""
 
         if size1 != size2 {
+            let minSize = min(size1, size2)
+
             for i in 0..<minSize {
-                ans += String(word1[i])
-                ans += String(word2[i])
+                ans += String(arr1[i])
+                ans += String(arr2[i])
             }
-            ans += size1 > size2 ? String(word1[minSize...size1 - 1]) : String(word2[minSize...size2 - 1])
+
+            ans += size1 > size2 ? String(arr1[minSize...size1 - 1]) : String(arr2[minSize...size2 - 1])
         } else {
             for i in 0..<size1 {
-                ans += String(word1[i])
-                ans += String(word2[i])
+                ans += String(arr1[i])
+                ans += String(arr2[i])
             }
         }
 
