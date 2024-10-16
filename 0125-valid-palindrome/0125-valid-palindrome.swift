72863b1d8d9d6b54f6ce1dce61299b7aa5705b94
다은 <down022@gmail.com>
2024-10-16 10:30:53 +0900

    Time: 18 ms (83.04%), Space: 18.6 MB (24.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.swift
+++ 0125-valid-palindrome/0125-valid-palindrome.swift
@@ -11,12 +11,10 @@ class Solution {
         }
         
         guard size > 0 else { return true }
-            
-        var p1 = 0
-        var p2 = size - 1
+
         for i in 0..<(size / 2) {
-            let c1 = alphanumeric[p1 + i].lowercased()
-            let c2 = alphanumeric[p2 - i].lowercased()
+            var c1 = alphanumeric[i].lowercased()
+            var c2 = alphanumeric[size - 1 - i].lowercased()
 
             if c1 != c2 {
                 return false
