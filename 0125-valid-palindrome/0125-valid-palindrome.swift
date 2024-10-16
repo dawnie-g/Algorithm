d7ca5d776f4a13427439c628eaf3e3d04ebee192
다은 <down022@gmail.com>
2024-10-16 10:26:38 +0900

    Time: 22 ms (60.52%), Space: 18.8 MB (17.65%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.swift
+++ 0125-valid-palindrome/0125-valid-palindrome.swift
@@ -1,40 +1,25 @@
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
         var alphanumeric: [Character] = []
-        var size = 0
 
         s.forEach {
             if $0.isLetter || $0.isNumber {
                 alphanumeric.append($0)
-                size += 1
             }
         }
+
+        let size = alphanumeric.count
         
         guard size > 0 else { return true }
             
         var p1 = 0
         var p2 = size - 1
+        for i in 0..<(size / 2) {
+            let c1 = alphanumeric[p1 + i].lowercased()
+            let c2 = alphanumeric[p2 - i].lowercased()
 
-        while p1 < p2 {
-            let c1 = alphanumeric[p1]
-            let c2 = alphanumeric[p2]
-
-            if c1.isLetter && c2.isLetter {
-                if c1.lowercased() != c2.lowercased() { return false }
-                p1 += 1
-                p2 -= 1
-            } else if c1.isNumber && c2.isNumber {
-                if c1 != c2 { return false }
-                p1 += 1
-                p2 -= 1
-            } else {
-                if !c1.isLetter && !c1.isNumber { 
-                    p1 += 1 
-                } else if !c2.isLetter && !c2.isNumber { 
-                    p2 -= 1 
-                } else {
-                    return false
-                }
+            if c1 != c2 {
+                return false
             }
         }
 
