16a0437983203510af2689dd63cfff50e4f1309b
다은 <down022@gmail.com>
2025-01-08 11:16:42 +0900

    Time: 25 ms (77.94%), Space: 18.2 MB (25.51%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.swift
+++ 0383-ransom-note/0383-ransom-note.swift
@@ -2,6 +2,8 @@ class Solution {
     func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
         var ransomDict:[Character: Int] = [:]
         var magazineDict:[Character: Int] = [:]
+        ransomDict.reserveCapacity(100_000)
+        magazineDict.reserveCapacity(100_000)
 
         ransomNote.forEach { char in
             ransomDict[char, default: 0] += 1
