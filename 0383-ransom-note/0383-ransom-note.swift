6ed90f74c460ca318a42c5d7884582572d1f888e
다은 <down022@gmail.com>
2025-01-08 11:14:08 +0900

    Time: 27 ms (74.29%), Space: 17.7 MB (80.36%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.swift
+++ 0383-ransom-note/0383-ransom-note.swift
@@ -1,11 +1,19 @@
 class Solution {
     func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
-        let ransomDict = Dictionary(grouping: ransomNote) { $0 }
-        let magazineDict = Dictionary(grouping: magazine) { $0 }
+        var ransomDict:[Character: Int] = [:]
+        var magazineDict:[Character: Int] = [:]
+
+        ransomNote.forEach { char in
+            ransomDict[char, default: 0] += 1
+        }
+
+        magazine.forEach { char in
+            magazineDict[char, default: 0] += 1
+        }
 
         for (key, value) in ransomDict {
             guard let magazineValue = magazineDict[key] else { return false }
-            if magazineValue.count < value.count { return false }
+            if magazineValue < value { return false }
         }
 
         return true
