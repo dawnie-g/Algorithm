12fc19ea10dcf4cafdc13c6ed0d575f91e53c85a
다은 <down022@gmail.com>
2025-01-16 10:57:51 +0900

    Time: 1792 ms (0%), Space: 21.9 MB (0%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.swift
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.swift
@@ -1,12 +1,10 @@
 class Solution {
     func xorAllNums(_ nums1: [Int], _ nums2: [Int]) -> Int {
-        let count1 = nums1.count
-        let count2 = nums2.count
         var ans: Int = 0
 
-        for i in 0..<count1 {
-            for j in 0..<count2 {
-                ans ^= nums1[i] ^ nums2[j]
+        nums1.forEach { n1 in
+            nums2.forEach { n2 in
+                ans ^= n1 ^ n2
             }
         }
 
