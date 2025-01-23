837ca9e4f7bbc9b7b6e0e3ec6e64790d374d4b54
다은 <down022@gmail.com>
2025-01-23 11:43:23 +0900

    Time: 0 ms (100%), Space: 19.9 MB (10%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.swift
+++ 1168-duplicate-zeros/1168-duplicate-zeros.swift
@@ -4,16 +4,32 @@ class Solution {
         var newArr = [Int]()
         newArr.reserveCapacity(arr.count)
         var newCnt = 0
+        var idx = 0
 
-        for i in 0..<arr.count where newCnt < cnt {
-            if arr[i] == 0 {
+        // for i in 0..<arr.count where newCnt < cnt {
+        //     if arr[i] == 0 {
+        //         newArr.append(0)
+        //         newCnt += 1
+        //     }
+        //     if newCnt < cnt {
+        //         newArr.append(arr[i])
+        //         newCnt += 1
+        //     }
+        // }
+
+        while newCnt < cnt {
+            if arr[idx] == 0 {
                 newArr.append(0)
                 newCnt += 1
-            }
-            if newCnt < cnt {
-                newArr.append(arr[i])
+                if newCnt < cnt {
+                    newArr.append(0)
+                    newCnt += 1
+                }
+            } else {
+                newArr.append(arr[idx])
                 newCnt += 1
             }
+            idx += 1
         }
 
         arr = newArr
