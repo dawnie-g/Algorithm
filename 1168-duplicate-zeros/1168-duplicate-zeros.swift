1b5401159976877734f4339825b6a6158007a0ef
다은 <down022@gmail.com>
2025-01-23 11:51:54 +0900

    Time: 0 ms (100%), Space: 19.8 MB (17.78%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.swift
+++ 1168-duplicate-zeros/1168-duplicate-zeros.swift
@@ -1,37 +1,15 @@
 class Solution {
     func duplicateZeros(_ arr: inout [Int]) {
-        let cnt = arr.count
-        var newArr = [Int]()
-        newArr.reserveCapacity(arr.count)
-        var newCnt = 0
-        var idx = 0
+        var queue = [Int]()
+        var p = 0
 
-        // for i in 0..<arr.count where newCnt < cnt {
-        //     if arr[i] == 0 {
-        //         newArr.append(0)
-        //         newCnt += 1
-        //     }
-        //     if newCnt < cnt {
-        //         newArr.append(arr[i])
-        //         newCnt += 1
-        //     }
-        // }
-
-        while newCnt < cnt {
-            if arr[idx] == 0 {
-                newArr.append(0)
-                newCnt += 1
-                if newCnt < cnt {
-                    newArr.append(0)
-                    newCnt += 1
-                }
-            } else {
-                newArr.append(arr[idx])
-                newCnt += 1
+        for i in 0..<arr.count {
+            if arr[i] == 0 {
+                queue.append(0)
             }
-            idx += 1
+            queue.append(arr[i])
+            arr[i] = queue[p]
+            p += 1
         }
-
-        arr = newArr
     }
 }
