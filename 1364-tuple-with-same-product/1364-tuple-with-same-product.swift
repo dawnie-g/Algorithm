4943ca21631b3bf5f61013e08efb4ac2fb4f0fdc
다은 <down022@gmail.com>
2025-02-06 13:53:28 +0900

    Time: 109 ms (100%), Space: 34.2 MB (100%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func tupleSameProduct(_ nums: [Int]) -> Int {
+        var dict: [Int: Int] = [:]
+
+        for i in 0..<nums.count-1 {
+            for j in i + 1..<nums.count {
+                dict[nums[i] * nums[j], default: 0] += 1
+            }
+        }
+
+        return dict.values.reduce(into: 0) { if $1 > 1 { $0 += $1 * 4 * ($1 - 1) } }
+    }
+}
