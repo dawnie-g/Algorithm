563b0c1965a47dc81bb770a23e5c9e94c9f54bf1
다은 <down022@gmail.com>
2025-02-19 10:30:24 +0900

    Time: 2 ms (66.67%), Space: 19.4 MB (33.33%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.swift
@@ -0,0 +1,29 @@
+class Solution {
+    func getHappyString(_ n: Int, _ k: Int) -> String {
+        let happyChar: [Character] = ["a", "b", "c"]
+        var happyCount = 0
+        var result = ""
+
+        func happyBT(_ str: String, _ count: Int) {
+            if count == n {
+                happyCount += 1
+                if happyCount == k {
+                    result = str
+                }
+                return
+            }
+
+            for char in happyChar {
+                guard result.isEmpty else { return }
+
+                if str.isEmpty || str.last != char {
+                    happyBT(str + "\(char)", count + 1)
+                }
+            }
+        }
+
+        happyBT("", 0)
+        
+        return result
+    }
+}
