b673c5fdcfa6cade52fc1e97551f1db5fb6fda57
다은 <down022@gmail.com>
2025-02-20 23:37:25 +0900

    Time: 22 ms (31.25%), Space: 19.5 MB (65.14%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        let n = nums.count
+        var ans = [0, 0]
+
+        for i in 0..<n {
+            for j in i + 1..<n where nums[i] + nums[j] == target {
+                ans = [i, j]
+                break
+            }
+        }
+
+        return ans
+    }
+}
