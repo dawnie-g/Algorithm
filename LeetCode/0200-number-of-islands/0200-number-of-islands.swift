23d66eccbf2ad667a6c69091682579b4c6672e17
KwakDADA <down022@gmail.com>
2025-02-20 23:55:53 +0900

    move to LeetCode

--- LeetCode/0200-number-of-islands/0200-number-of-islands.swift
+++ LeetCode/0200-number-of-islands/0200-number-of-islands.swift
@@ -2,12 +2,9 @@ class Solution {
     func numIslands(_ grid: [[Character]]) -> Int {
         let m = grid.count
         let n = grid[0].count
-
-        var visited = [[Bool]](repeating: [Bool](repeating: false, count: n), count: m)
         let directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        var visited = [[Bool]](repeating: [Bool](repeating: false, count: n), count: m)
         var island = 0
-
-        
 
         for i in 0..<m {
             for j in 0..<n where !visited[i][j] && grid[i][j] == "1" {
@@ -15,21 +12,23 @@ class Solution {
                 var head = 0
                 var tail = 1
 
-                while head < tail {
+                while head != tail {
                     let curr = queue[head]
                     head += 1
                     visited[curr.0][curr.1] = true
-
+                    
                     for dir in directions {
-                    let dx = curr.0 + dir.0
-                    let dy = curr.1 + dir.1
+                        let x = curr.0 + dir.0
+                        let y = curr.1 + dir.1
 
-                    guard 0..<m ~= dx && 0..<n ~= dy && !visited[dx][dy] && grid[dx][dy] == "1" else { continue }
-                        queue.append((dx, dy))
-                        tail += 1
-                        visited[dx][dy] = true
-                    }
+                        if 0..<m ~= x && 0..<n ~= y && grid[x][y] == "1" && !visited[x][y] {
+                            queue.append((x, y))
+                            tail += 1
+                            visited[x][y] = true
+                        }
+                    }         
                 }
+
                 island += 1
             }
         }
