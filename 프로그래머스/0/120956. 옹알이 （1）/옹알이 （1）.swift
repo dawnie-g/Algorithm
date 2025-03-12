5bf841b3e5ea716cd5aea7907f3477e42d056381
다은 <down022@gmail.com>
2025-03-12 19:39:34 +0900

    [level 0] Title: 옹알이 (1), Time: 2.24 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.swift
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.swift
@@ -1,16 +1,11 @@
 import Foundation
 
 func solution(_ babbling:[String]) -> Int {
-    let words = ["aya", "ye", "woo", "ma"]
-    var babbling = babbling.filter{$0.count > 1}
-    var count: Int = 0
-
-    for i in 0..<babbling.count {
-        for word in words {
-            babbling[i] = babbling[i].replacingOccurrences(of: word, with: " ")
+    return babbling.filter { 
+        var word = $0
+        ["aya", "ye", "woo", "ma"].forEach {
+            word = word.replacingOccurrences(of: $0, with: " ")
         }
-        if babbling[i].allSatisfy { $0 == " "} { count += 1 }
-    }
-
-    return count
+        return word.allSatisfy { $0 == " " }
+    }.count
 }
