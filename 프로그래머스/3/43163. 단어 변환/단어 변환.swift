34d0c864234465d6566ead11623d765bf8bfc1f0
다은 <down022@gmail.com>
2025-03-19 09:35:09 +0900

    [level 3] Title: 단어 변환, Time: 0.27 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
+    var ans: Int = Int.max
+    var visited: [Bool] = [Bool](repeating: false, count: words.count)
+    
+    func dfs(_ currIndex: Int, _ count: Int) {
+        let currWord = currIndex == -1 ? begin : words[currIndex]
+        // print("currWord: \(currWord)")
+        // print("count: \(count)")
+        
+        // 현재 단어가 타겟과 같으면 ans와 count를 비교, 더 작은 값을 ans에 할당
+        if currWord == target {
+            ans = min(count, ans)
+            return
+        }
+        
+        // 방문하지 않은 단어와 현재 단어를 비교
+        outer: for i in 0..<words.count where visited[i] == false {
+            // print("비교 대상: \(words[i])")
+            var diffChar = false
+            
+            // 한 글자씩 비교해가며 다른 글자가 두 개 나오면 비교를 끝내고 다음 단어로 넘어감
+            for (char1, char2) in zip(currWord, words[i]) where char1 != char2 {
+                if !diffChar { 
+                    diffChar = true
+                } else {
+                    // print("다음 단어")
+                    continue outer
+                }
+            }
+            
+            // 다른 글자가 하나 있었으면 그 단어로 dfs를 돌림
+            if diffChar {
+                visited[i] = true
+                dfs(i, count + 1)
+                visited[i] = false
+            } else { 
+                // 다른 글자가 하나도 없는 경우 -> 제한 사항에 의해 존재하지 않음
+            }
+        }
+        return
+    }
+    
+    dfs(-1, 0)
+    
+    return ans == Int.max ? 0 : ans
+}
