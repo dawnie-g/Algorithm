16aa784b513def5c3d64c1deac496b597e3cdca1
다은 <down022@gmail.com>
2025-03-24 19:29:08 +0900

    [level 3] Title: 이중우선순위큐, Time: 3435.74 ms, Memory: 22.1 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.swift
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.swift
@@ -1,26 +1,71 @@
 import Foundation
 
-func solution(_ operations:[String]) -> [Int] {
-    var queue: [Int] = []
-    
-    for operation in operations {
-        let operation = operation.components(separatedBy: " ")
-        let command = operation[0]
-        guard let data = Int(operation[1]) else { continue }
-        
-        switch command {
-            case "I":
-            queue.append(data)
-            
-            case "D":
-            guard !queue.isEmpty else { continue }
-            guard let value = data == 1 ? queue.max() : queue.min(),
-                  let index = queue.firstIndex(of: value) else { continue }
-            queue.remove(at: index)
-            
-            default: continue
-        }
+struct Operation {
+  let command: Command
+  let number: Int
+}
+
+enum Command: String {
+  case insert = "I"
+  case delete = "D"
+}
+
+struct DoublePriorityQueue {
+  private var _elements = [Int]()
+
+  mutating func insert(_ number: Int) {
+    _elements.append(number)
+  }
+
+  mutating func deleteMaximum() {
+    guard !_elements.isEmpty else { return }
+    let max = _elements.max()!
+    let index = _elements.firstIndex { $0 == max }
+    _elements.remove(at: index!)
+  }
+
+  mutating func deleteMinimum() {
+    guard !_elements.isEmpty else { return }
+    let min = _elements.min()!
+    let index = _elements.firstIndex { $0 == min }
+    _elements.remove(at: index!)
+  }
+
+  var elements: [Int] {
+    if _elements.isEmpty {
+      return [0, 0]
+    } else {
+      let maximum = _elements.max()!
+      let minimum = _elements.min()!
+      return [maximum, minimum]
     }
-    
-    return [queue.max() ?? 0, queue.min() ?? 0]
+  }
+}
+
+func solution(_ operations:[String]) -> [Int] {
+  var operationStructs = [Operation]()
+  for operation in operations {
+    let splitted = operation.components(separatedBy: " ")
+    let command = Command(rawValue: splitted[0])!
+    let number = Int(splitted[1])!
+    let operationStruct = Operation(command: command, number: number)
+    operationStructs.append(operationStruct)
+  }
+
+  var queue = DoublePriorityQueue()
+
+  for operationStruct in operationStructs {
+    switch operationStruct.command {
+    case .insert:
+      queue.insert(operationStruct.number)
+    case .delete:
+      if operationStruct.number == 1 {
+        queue.deleteMaximum()
+      } else {
+        queue.deleteMinimum()
+      }
+    }
+  }
+
+  return queue.elements
 }
