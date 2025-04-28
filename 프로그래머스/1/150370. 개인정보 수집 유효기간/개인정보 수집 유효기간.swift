b936b8b6e2c4f49e02329428ddde50f178127909
다은 <down022@gmail.com>
2025-04-28 10:11:43 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 2.26 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.swift
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.swift
@@ -1,47 +1,39 @@
 import Foundation
 
+/*
+
+*/
+
 func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
-    let n = privacies.count
-    let today = today.split(separator: ".").map { Int(String($0))! }
-    var termDict = [String: Int]()
+    let today = today.components(separatedBy: ".").map { Int($0)! }
+    var period = [String: Int]()
     var expired = [Int]()
     
     for term in terms {
-        let term = term.split(separator: " ").map { String($0) }
-        let name = term[0]
-        let period = Int(term[1])!
-        
-        termDict[name] = period
+        let term = term.components(separatedBy: " ")
+        period[term[0]] = Int(term[1])!
     }
     
-    for i in 0..<n {
-        let privacy = privacies[i].split(separator: " ").map { String($0) }
-        var date = privacy[0].split(separator: ".").map { Int($0)! }
-        let term = termDict[privacy[1]] ?? 0
-        
-        let year = term / 12
-        let month = term % 12
-        date[0] += year
-        date[1] += month
-        
-        if date[1] > 12 {
-            date[0] += 1
-            date[1] %= 12
+    for i in 0..<privacies.count {
+        let privacy = privacies[i].components(separatedBy: " ")
+        let date = privacy[0].components(separatedBy: ".").map { Int($0)! } // year, month, day
+        let period = period[privacy[1]]!
+        var expYear = date[0] + period / 12
+        var expMonth = date[1] + period % 12
+        let expDay = date[2] - 1
+        if expMonth > 12 {
+            expYear += expMonth / 12
+            expMonth %= 12
         }
         
-        if today[0] > date[0] {
+        if today[0] > expYear {
             expired.append(i+1)
-        } else if today[0] == date[0] {
-            if today[1] > date[1] {
-                expired.append(i+1)
-            } else if today[1] == date[1] {
-                if today[2] >= date[2] {
-                    expired.append(i+1)
-                }
-            }
+        } else if today[0] == expYear && today[1] > expMonth {
+            expired.append(i+1)
+        } else if today[0] == expYear && today[1] == expMonth && today[2] > expDay {
+            expired.append(i+1)
         }
     }
-    
     
     return expired
 }
