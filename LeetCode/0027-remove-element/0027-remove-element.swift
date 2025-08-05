d32f6d51c44768e13b68363386e56e32c22f5882
다은 <down022@gmail.com>
2025-08-05 19:21:42 +0900

    Time: 0 ms (100%), Space: 19.3 MB (80.94%) - LeetHub

--- LeetCode/0027-remove-element/0027-remove-element.swift
+++ LeetCode/0027-remove-element/0027-remove-element.swift
@@ -1,13 +1,14 @@
 class Solution {
     func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
-        var cnt = 0
-
-        for i in 0..<nums.count where nums[i] != val {
-            cnt += 1
+        var n = nums.count
+        var eqaul = 0
+        
+        for i in 0..<n where nums[i] == val {
+            eqaul += 1
         }
 
-        nums = nums.filter { $0 != val }
-        
-        return cnt
+        nums.sort { $0 != val && $1 == val }
+
+        return n - eqaul
     }
 }
