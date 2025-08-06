59dbf1c244f96b713df9bc17dd343f1ea035b752
다은 <down022@gmail.com>
2025-08-06 15:35:51 +0900

    Time: 51 ms (62%), Space: 17.1 MB (86%) - LeetHub

--- LeetCode/0941-valid-mountain-array/0941-valid-mountain-array.swift
+++ LeetCode/0941-valid-mountain-array/0941-valid-mountain-array.swift
@@ -9,30 +9,26 @@ class Solution {
         var acrossTop = false
 
         for i in 1..<len {
-            if !increase && !acrossTop && arr[i] > prev {
+            if !increase && !acrossTop && arr[i] > arr[i-1] {
                 increase = true
             }
 
-            if !acrossTop && arr[i] < prev {
+            if !acrossTop && arr[i] < arr[i-1] {
                 acrossTop = true
             }
 
-            if !decrease && acrossTop && arr[i] < prev {
+            if !decrease && acrossTop && arr[i] < arr[i-1] {
                 decrease = true
             }
 
-            let beforeTopDecreasing = !acrossTop && arr[i] < prev
-            let afterTopIncreasing = acrossTop && arr[i] > prev
-            let notStrictly = arr[i] == prev
+            let beforeTopDecreasing = !acrossTop && arr[i] < arr[i-1]
+            let afterTopIncreasing = acrossTop && arr[i] > arr[i-1]
+            let notStrictly = arr[i] == arr[i-1]
             
             if beforeTopDecreasing || afterTopIncreasing || notStrictly {
                 return false
             }
-
-            prev = arr[i]
         }
-
-        print(increase, decrease)
 
         return increase && decrease
     }
