2338b70072afe84c34776212336ec111546be0d4
다은 <down022@gmail.com>
2025-08-07 13:48:19 +0900

    Time: 0 ms (100%), Space: 20.2 MB (35.92%) - LeetHub

--- LeetCode/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array-in-Place.swift
+++ LeetCode/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array-in-Place.swift
@@ -1,12 +1,12 @@
 class Solution {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
         var k = 1
-        
-        for i in 1..<nums.count where nums[i] != nums[k-1] {
-            nums[k] = nums[i]
+
+        for i in 0..<nums.count-1 where nums[i] != nums[i+1] {
+            nums[k] = nums[i+1]
             k += 1
         }
-        
+
         return k
     }
 }
