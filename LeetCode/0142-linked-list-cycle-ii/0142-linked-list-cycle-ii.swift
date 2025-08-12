b40a3fc4605f9585f298e1899e7420157361d37e
다은 <down022@gmail.com>
2025-08-12 10:11:13 +0900

    Time: 14 ms (96.34%), Space: 17.8 MB (71.95%) - LeetHub

---
+++ LeetCode/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.swift
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func detectCycle(_ head: ListNode?) -> ListNode? {
+        var slow = head
+        var fast = head
+
+        while slow != nil {
+            if slow == nil && fast == nil { return nil }
+            
+            slow = slow?.next
+            fast = fast?.next?.next
+
+            if slow === fast { break }
+        }
+
+        var pos = head
+        while fast !== pos {
+            pos = pos?.next
+            fast = fast?.next
+        }
+
+        return pos
+    }
+}
