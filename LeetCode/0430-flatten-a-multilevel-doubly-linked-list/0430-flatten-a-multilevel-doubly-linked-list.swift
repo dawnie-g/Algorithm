706a3e6970165e203d42a984bc989d7348ce750d
다은 <down022@gmail.com>
2025-08-15 17:32:05 +0900

    Time: 0 ms (100%), Space: 19.2 MB (23.81%) - LeetHub

---
+++ LeetCode/0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.swift
@@ -0,0 +1,40 @@
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var prev: Node?
+ *     public var next: Node?
+ *     public var child: Node?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.prev = nil
+ *         self.next = nil
+ *         self.child  = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func flatten(_ head: Node?) -> Node? {
+        var stack: [Node?] = []
+        var curr = head
+
+        while curr != nil {
+            if let child = curr?.child {
+                stack.append(curr?.next)
+                curr?.child = nil
+                curr?.next = child
+                child.prev = curr
+                curr = child
+            } else if curr?.next == nil, !stack.isEmpty {
+                let parent = stack.removeLast()
+                curr?.next = parent
+                parent?.prev = curr
+            } else {
+                curr = curr?.next
+            }
+        }
+
+        return head
+    }
+}
