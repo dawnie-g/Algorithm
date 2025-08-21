a1c12ae7669b9df0ca11d9697d94846c58f63c36
다은 <down022@gmail.com>
2025-08-21 09:21:05 +0900

    Time: 17 ms (59.09%), Space: 22.2 MB (78.66%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.swift
@@ -0,0 +1,12 @@
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        var numsSet = Set<Int>()
+
+        for num in nums {
+            if numsSet.contains(num) { return true }
+            numsSet.insert(num)
+        }
+
+        return false
+    }
+}
