e28f1cff4f022447683ca1a2b33c79f0c4ba7cbd
다은 <down022@gmail.com>
2025-09-03 10:20:51 +0900

    Time: 2 ms (14.39%), Space: 22.9 MB (5.71%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string-recursion-head-tail.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func reverseString(_ s: inout [Character]) {
+        recursion(&s, 0, s.count - 1)
+    }
+
+    private func recursion(_ s: inout [Character], _ head: Int, _ tail: Int) {
+        if head >= tail { 
+            return 
+        }
+        recursion(&s, head + 1, tail - 1)
+        s.swapAt(head, tail)
+    }
+}
