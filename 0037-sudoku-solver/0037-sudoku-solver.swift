b3c61935e8f33455a602bd409265156bded66b37
다은 <down022@gmail.com>
2025-09-25 15:01:09 +0900

    Time: 1436 ms (16.13%), Space: 20.4 MB (15.73%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.swift
+++ 0037-sudoku-solver/0037-sudoku-solver.swift
@@ -1,17 +1,5 @@
 class Solution {
     func solveSudoku(_ board: inout [[Character]]) {
-        var numsInRow: [Int: [Character]] = [:]
-        var numsInCol: [Int: [Character]] = [:]
-        var numsInBox: [Int: [Character]] = [:]
-        for r in 0..<9 {
-            for c in 0..<9 where board[r][c].isNumber {
-                numsInRow[r, default: []].append(board[r][c])
-                numsInCol[c, default: []].append(board[r][c])
-                let boxIndex = (r / 3 * 3) + (c / 3)
-                numsInBox[boxIndex, default: []].append(board[r][c])
-            }
-        }
-
         backtracking(0, 0)
 
         func backtracking(_ row: Int, _ col: Int) -> Bool {
@@ -24,23 +12,23 @@ class Solution {
 
             for num in "123456789" where isValid(row, col, num) {
                 board[row][col] = num
-                numsInRow[row, default: []].append(num)
-                numsInCol[col, default: []].append(num)
-                numsInBox[boxIndex, default: []].append(num)
                 if backtracking(nextRow, nextCol) { return true }
                 board[row][col] = "."
-                numsInRow[row] = numsInRow[row]!.filter { $0 != num }
-                numsInCol[col] = numsInCol[col]!.filter { $0 != num }
-                numsInBox[boxIndex] = numsInBox[boxIndex]!.filter { $0 != num }
             }
             return false
         }
 
         func isValid(_ row: Int, _ col: Int, _ num: Character) -> Bool {
-            let boxIndex = row / 3 * 3 + col / 3
-            return !numsInRow[row, default: []].contains(num) 
-                && !numsInCol[col, default: []].contains(num) 
-                && !numsInBox[boxIndex, default: []].contains(num)
+            if board[row].contains(num) { return false }
+            if (0..<9).map { board[$0][col] }.contains(num) { return false }
+            
+            for r in (row / 3 * 3)..<(row / 3 + 1) * 3 {
+                for c in (col / 3 * 3)..<(col / 3 + 1) * 3 where board[r][c] == num {
+                    return false
+                }
+            }
+
+            return true
         }
     }
 
